9056062aee819b08f537c37fa2115d06a55d0ca9
bashfulrobot <dustin@bashfulrobot.com>
2025-09-24 23:40:43 -0700

    feat(flake): ✨ restructure flake with modular architecture
    - Add stylix input for system-wide theming
    - Create nixosModules and homeManagerModules exports
    - Add helper functions mkHyprlandSystem and mkHyprlandHome
    - Include all necessary inputs: hyprland, home-manager, cachix, stylix
    - Set up modular module structure for easy consumption

--- flake.nix
+++ flake.nix
@@ -1,45 +1,85 @@
 {
-  description = "Hyprland configuration for NixOS";
+  description = "Reusable Hyprland flake with cachix and home-manager support";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-
-    home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     hyprland = {
       url = "github:hyprwm/Hyprland";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    cachix = {
+      url = "github:cachix/cachix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, hyprland, ... }@inputs:
+  outputs = { self, nixpkgs, hyprland, home-manager, cachix, stylix, ... }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosModules = {
+        hyprland = import ./modules/nixos/hyprland.nix;
+        cachix = import ./modules/nixos/cachix.nix;
+        stylix = import ./modules/nixos/stylix.nix;
+        dconf = import ./modules/nixos/dconf.nix;
+        xdg = import ./modules/nixos/xdg.nix;
       };
-    in {
-      nixosModules.default = { config, lib, ... }: {
-        imports = [ ./configuration.nix ];
 
-        nix = {
-          settings = {
-            experimental-features = [ "nix-command" "flakes" ];
-            substituters =
-              [ "https://cache.nixos.org" "https://hyprland.cachix.org" ];
-            trusted-public-keys = [
-              "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-            ];
+      homeManagerModules = {
+        hyprland = import ./modules/home-manager/hyprland.nix;
+        stylix = import ./modules/home-manager/stylix.nix;
+        dconf = import ./modules/home-manager/dconf.nix;
+        xdg = import ./modules/home-manager/xdg.nix;
+      };
+
+      lib = {
+        mkHyprlandSystem = { extraModules ? [] }:
+          nixpkgs.lib.nixosSystem {
+            inherit system;
+            modules = [
+              hyprland.nixosModules.default
+              home-manager.nixosModules.home-manager
+              stylix.nixosModules.stylix
+              self.nixosModules.hyprland
+              self.nixosModules.cachix
+              self.nixosModules.stylix
+              self.nixosModules.dconf
+              self.nixosModules.xdg
+            ] ++ extraModules;
           };
-        };
+
+        mkHyprlandHome = { extraModules ? [] }:
+          home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [
+              hyprland.homeManagerModules.default
+              stylix.homeManagerModules.stylix
+              self.homeManagerModules.hyprland
+              self.homeManagerModules.stylix
+              self.homeManagerModules.dconf
+              self.homeManagerModules.xdg
+            ] ++ extraModules;
+            extraSpecialArgs = { inherit hyprland stylix; };
+          };
       };
 
-      homeManagerModules.default = { config, lib, ... }: {
-        imports = [ hyprland.homeManagerModules.default ./home.nix ];
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ nixpkgs-fmt nil ];
       };
     };
 }

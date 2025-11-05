c4e777a10ae246c9d7cf1fa51ce5b05cda7583fe
Dustin Krysak <dustin@bashfulrobot.com>
2025-11-05 08:53:16 -0800

    refactor(waybar): ♻️ extract CSS to separate file
    Move waybar styling to style.nix for better organization while keeping GTK theming variables inline

---
+++ modules/desktop/waybar/style.nix
@@ -0,0 +1,71 @@
+''
+  * {
+    font-family: "JetBrainsMono Nerd Font";
+    font-size: 11px;
+    margin: 0px;
+    padding: 0px;
+  }
+
+  window#waybar {
+    background: @theme_base_color;
+    border-radius: 0px;
+  }
+
+  tooltip {
+    border-radius: 8px;
+  }
+
+  .modules-left, .modules-center, .modules-right {
+    background: transparent;
+    padding: 0 15px;
+  }
+
+  #workspaces, #idle_inhibitor, #clock, #bluetooth, #pulseaudio, #battery, #tray, #custom-notification {
+    padding: 1px 10px;
+    margin: 0 1.5px;
+  }
+
+  #workspaces button {
+    padding: 1px 8px;
+    margin: 0 1px;
+    border-radius: 0px;
+    background-color: transparent;
+    transition: all 0.2s ease-in-out;
+  }
+
+  #workspaces button:hover {
+    background-color: @surface1;
+  }
+
+  #workspaces button.active {
+    background-color: @blue;
+    font-weight: bold;
+  }
+
+  #clock {
+    color: @yellow;
+    font-weight: 500;
+  }
+
+  #battery.critical {
+    color: @red;
+    font-weight: bold;
+  }
+
+  #battery.charging {
+    color: @blue;
+  }
+
+  #pulseaudio {
+    color: @lavender;
+  }
+
+  #bluetooth {
+    color: @blue;
+  }
+
+  #custom-power {
+    color: @red;
+    padding: 2px 8px;
+  }
+''

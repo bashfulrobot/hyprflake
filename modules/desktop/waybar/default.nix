b2df4b6723ef6cbb3b43d9c92832c26611ea1c33
Dustin Krysak <dustin@bashfulrobot.com>
2025-12-11 14:23:24 -0800

    feat(waybar): ✨ show only workspaces with open applications

--- modules/desktop/waybar/default.nix
+++ modules/desktop/waybar/default.nix
@@ -83,19 +83,6 @@ in
             on-click = "activate";
             show-special = false;
             format = "{icon}";
-            # Ensure workspaces 1-10 always exist
-            persistent-workspaces = {
-              "1" = [ ];
-              "2" = [ ];
-              "3" = [ ];
-              "4" = [ ];
-              "5" = [ ];
-              "6" = [ ];
-              "7" = [ ];
-              "8" = [ ];
-              "9" = [ ];
-              "10" = [ ];
-            };
           };
 
           "idle_inhibitor" = {
